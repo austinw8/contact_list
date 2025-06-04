5020d45fc13884cdf442e4b6d5a7271033ae8520
Austin Mangelson <austin.mangelson@legrandehealth.com>
2025-06-04 09:47:42 -0500

    contact form created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,28 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import ContactList from "./ContactList";
+import ContactForm from "./ContactForm";
 
 function App() {
-  const [contacts, setContacts] = useState([])
+  const [contacts, setContacts] = useState([]);
 
   useEffect(() => {
-    fetchContacts()
-  }, [])
+    fetchContacts();
+  }, []);
 
   const fetchContacts = async () => {
-    const response = await fetch("http://127.0.0.1:5000/contacts")
-    const data=await response.json()
-    setContacts(data.contacts)
-    console.log(data.contacts)
-  }
+    const response = await fetch("http://127.0.0.1:5000/contacts");
+    const data = await response.json();
+    setContacts(data.contacts);
+    console.log(data.contacts);
+  };
 
   return (
     <>
+      <ContactList contacts={contacts} />
+      <ContactForm />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
